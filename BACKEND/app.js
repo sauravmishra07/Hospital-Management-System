cc46588df2c6d5cc8de06196174216c94be54363
Saurav mishra <mishrasaurav499@gmail.com>
2025-10-06 13:55:39 +0530

    User Backend & Auth Middlewares

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -6,6 +6,7 @@ import fileUpload from 'express-fileupload';
 import { connectDB } from './database/dbconnection.js';
 import messageRouter from './router/messageRouter.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import userRouter from "./router/userRouter.js";
 
 const app = express();
 config({ path: './config/config.env'});
@@ -29,6 +30,7 @@ app.use(fileUpload({
 }));
 
 app.use('/api/v1/message', messageRouter);
+app.use("/api/v1/user", userRouter);
 
 connectDB();
 app.use(errorMiddleware);
--- BACKEND/middlewares/errorMiddleware.js
+++ BACKEND/middlewares/errorMiddleware.js
@@ -5,13 +5,15 @@ class ErrorHandler extends Error {
   }
 }
 
+export { ErrorHandler }; // <-- Add this line
+
 export const errorMiddleware = (err, req, res, next) => {
   err.message = err.message || "Internal Server Error";
   err.statusCode = err.statusCode || 500;
 
   if (err.code === 11000) {
-    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`,
-      err = new ErrorHandler(message, 400);
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new ErrorHandler(message, 400);
   }
   if (err.name === "JsonWebTokenError") {
     const message = `Json Web Token is invalid, Try again!`;
@@ -22,8 +24,8 @@ export const errorMiddleware = (err, req, res, next) => {
     err = new ErrorHandler(message, 400);
   }
   if (err.name === "CastError") {
-    const message = `Invalid ${err.path}`,
-      err = new ErrorHandler(message, 400);
+    const message = `Invalid ${err.path}`;
+    err = new ErrorHandler(message, 400);
   }
 
   const errorMessage = err.errors
@@ -34,7 +36,6 @@ export const errorMiddleware = (err, req, res, next) => {
 
   return res.status(err.statusCode).json({
     success: false,
-    // message: err.message,
     message: errorMessage,
   });
 };
