64953cef4f72e5f36726c1e0cdb2b4cd307f860e
Saurav mishra <mishrasaurav499@gmail.com>
2025-10-07 14:46:28 +0530

    Backend complete

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -7,6 +7,7 @@ import { connectDB } from './database/dbconnection.js';
 import messageRouter from './router/messageRouter.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
 import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
 
 const app = express();
 config({ path: './config/config.env'});
@@ -31,7 +32,7 @@ app.use(fileUpload({
 
 app.use('/api/v1/message', messageRouter);
 app.use("/api/v1/user", userRouter);
-
+app.use("/api/v1/appointment", appointmentRouter);
 connectDB();
 app.use(errorMiddleware);
 export default app;
